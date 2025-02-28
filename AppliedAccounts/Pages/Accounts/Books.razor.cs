509851b0dbe6c098687682e167b11de0ecb82ffc
appliedsoftwarehouse <info@jahangir.com>
2025-03-01 00:17:52 +0300

    HPLT - 01-Mar-2025. Net Core 9. working

--- AppliedAccounts/Pages/Accounts/Books.razor.cs
+++ AppliedAccounts/Pages/Accounts/Books.razor.cs
@@ -1,5 +1,6 @@
 ﻿using AppliedAccounts.Models;
 using AppliedDB;
+using System.Data;
 using System.Linq.Expressions;
 
 namespace AppliedAccounts.Pages.Accounts
@@ -7,25 +8,32 @@ namespace AppliedAccounts.Pages.Accounts
     public partial class Books
     {
 
-        public AppUserModel userProfile { get; set; }
+        public AppUserModel UserProfile { get; set; }
         public BookModel MyModel { get; set; } = new();
-        public List<CodeTitle> BookList { get; set; }
+        public List<CodeTitle> BookList { get; set; } = new();
         public int BookNature { get; set; }
-
-
+        public DateTime DT_Start { get; set; }
+        public DateTime DT_End { get; set; }
+        public string SearchText { get; set; }
+        
+        public BookRec BookRecord {get; set;}
+        public List<BookRec> BookRecords {get; set;}
 
         public Books()
         {
-            MyModel = new(0, userProfile ?? new());
+            MyModel = new(0, UserProfile ?? new());
+           
+
         }
 
 
         public void Back() { NavManager.NavigateTo("/Menu/Accounts"); }
 
-        public void GetBookList(int _BookNature)
+        public List<CodeTitle> GetBookList(int _BookNature)
         {
-            MyModel.Source = new(userProfile);
-            BookList = MyModel.Source.GetBookAccounts(_BookNature);
+            MyModel.Source = new(UserProfile);
+            var _BookList = MyModel.Source.GetBookAccounts(_BookNature) ?? new(); 
+            return _BookList;
 
         }
 
@@ -55,11 +63,23 @@ namespace AppliedAccounts.Pages.Accounts
         //    }
         //}
         #endregion
+
+        public string GetTitle(List<CodeTitle> _List, int _Value)
+        {
+            if (_List.Count == 0) { return string.Empty; }
+            if (_List is null) { return string.Empty; }
+            return _List.Where(x => x.ID == _Value).Select(x => x.Title).First();
+        }
     }
 
-    public enum ChooseBook
+    public class BookRec
     {
-        Cash = 1,
-        Bank = 2
+        public int Vou_No { get; set; }
+        public DateTime Vou_Date { get; set; }
+        public int Description { get; set; }
+        public string Recevied { get; set; }
+        public decimal Paid { get; set; }
+        public decimal Balance { get; set; }
     }
+
 }
