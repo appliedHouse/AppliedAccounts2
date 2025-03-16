5e36b278626b91960916b0794cb519f5b995b2e7
Mohammed Amir Jahangir <m.amir@modtechno.com>
2025-03-16 13:41:19 +0300

    MTS - 16-Mar-2025. miner changed update

--- AppliedDB/AppliedDB/TempDB.cs
+++ AppliedDB/AppliedDB/TempDB.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
+﻿using System.Data;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AppliedDB
 {
@@ -28,18 +22,26 @@ namespace AppliedDB
             TempTable = new DataTable();
             await Task.Run(() =>
             {
-                TableName = _DataName;
-                if (MyConnection.State != ConnectionState.Open) { MyConnection.Open(); }
-
-                SQLiteCommand _Command = new(MyConnection);
-                _Command.CommandText = $"SELECT * FROM [{TableName}]";
-                DataSet _DataSet = new DataSet();
-                SQLiteDataAdapter _Adapter = new(_Command);
-                _Adapter.Fill(_DataSet, "SaleData");
-                if (_DataSet.Tables.Count > 0)
+                try
                 {
-                    TempTable = _DataSet.Tables[0];
+                    TableName = _DataName;
+                    if (MyConnection.State != ConnectionState.Open) { MyConnection.Open(); }
+
+                    SQLiteCommand _Command = new(MyConnection);
+                    _Command.CommandText = $"SELECT * FROM [{TableName}]";
+                    DataSet _DataSet = new DataSet();
+                    SQLiteDataAdapter _Adapter = new(_Command);
+                    _Adapter.Fill(_DataSet, "SaleData");
+                    if (_DataSet.Tables.Count > 0)
+                    {
+                        TempTable = _DataSet.Tables[0];
+                    }
                 }
+                catch (Exception)
+                {
+                    TempTable = new();
+                }
+
             });
             return TempTable;
         }
