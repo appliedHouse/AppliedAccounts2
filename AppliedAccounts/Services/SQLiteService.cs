9bcc04b936268e514474e6c5e4368af9cd28b8c1
appliedsoftwarehouse <info@jahangir.com>
2025-05-17 20:54:18 +0300

    HPLT - 17-May-2025. Update just after comment

---
+++ AppliedAccounts/Services/SQLiteService.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SQLite;
+
+namespace AppliedAccounts.Services
+{
+    public class SQLiteService
+    {
+        public AppliedDB.Connections MyConnections { get; set; }
+        public SQLiteService(AppliedDB.AppPaths _AppPaths)
+        {
+            MyConnections = new(_AppPaths);
+        }
+
+        public SQLiteConnection? GetSQLiteConnection(SQLiteConnectionType _ConnectionType)
+        {
+            switch (_ConnectionType)
+            {
+                case SQLiteConnectionType.Users:
+                    return MyConnections.GetSQLiteUsers();
+                case SQLiteConnectionType.Client:
+                    return MyConnections.GetSQLiteClient();
+                case SQLiteConnectionType.Language:
+                    return MyConnections.GetSQLiteLanguage();
+                case SQLiteConnectionType.Message:
+                    return MyConnections.GetSQLiteMessage();
+                case SQLiteConnectionType.System:
+                    return MyConnections.GetSQLiteSystem();
+                case SQLiteConnectionType.Session:
+                    return MyConnections.GetSQLiteSession();
+            }
+            return null;
+        }
+
+        // Changed the accessibility of the enum to public to fix CS0051
+        public enum SQLiteConnectionType
+        {
+            Users,
+            Client,
+            Language,
+            Message,
+            System,
+            Session
+        }
+    }
+}
